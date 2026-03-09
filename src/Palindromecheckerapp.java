87bbda7272c02bdf27a3e1a02f52b833808ec9b9
garima-jand <garimajand10@gmail.com>
2026-03-09 15:18:35 +0530

    UC2

--- src/Palindromecheckerapp.java
+++ src/Palindromecheckerapp.java
@@ -2,6 +2,36 @@ import java.util.Scanner;
 
 class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Palindrome Checker App!");
+        System.out.print("Enter a word: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("\"" + input + "\" is a palindrome!");
+        } else {
+            System.out.println("\"" + input + "\" is NOT a palindrome.");
+        }
+
+        scanner.close();
+    }
+
+    // Method to check if a string is a palindrome
+    public static boolean isPalindrome(String str) {
+        // Normalize the string: remove spaces and make lowercase
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
